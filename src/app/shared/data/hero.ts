441848af76dc23c301a3aa950b32a3bb1390f3c4
Victor <dumas.victor97@gmail.com>
2021-03-16 14:55:55 +0100

    c cassé

--- src/app/shared/data/hero.ts
+++ src/app/shared/data/hero.ts
@@ -1,13 +1,13 @@
 export class Hero {
   uuid?: string;
   name: string = '';
-  strength: number = 5;
-  rapidity: number = 5;
+  dodge: number = 5;
+  speed: number = 5;
   damage: number = 5;
   health: number = 5;
 
 
   public getLeftPoints() {
-    return 40 - this.strength - this.rapidity - this.damage - this.health;
+    return 40 - this.dodge - this.speed - this.damage - this.health;
   }
 }
