237cc4dec9ac59655c542914fa72b61d762a3b06
Victor <dumas.victor97@gmail.com>
2021-03-16 15:53:39 +0100

    edit create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CreateHeroComponent } from './modules/create-hero/create-hero.component
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { HeroDetailComponent } from './modules/hero-detail/hero-detail.component';
 import { HeroesComponent } from './modules/heroes/heroes.component';
+import { EditHeroComponent } from './modules/edit-hero/edit-hero.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'create', component: CreateHeroComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'edit/:id', component: EditHeroComponent  },
 ];
 
 @NgModule({
