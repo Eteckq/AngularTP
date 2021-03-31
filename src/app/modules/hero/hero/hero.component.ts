800b506b34ff0846fe79e4de8cd2707bf6280915
Yohan <yohan.g99@gmail.com>
2021-03-31 14:50:50 +0200

    stats according to weapon

--- src/app/modules/hero/hero/hero.component.ts
+++ src/app/modules/hero/hero/hero.component.ts
@@ -12,4 +12,44 @@ import { Location } from '@angular/common';
 export class HeroComponent {
   @Input()
   hero: Hero;
+
+  getDamage() {
+    if (this.hero.weapon?.damage !== 0) {
+      if (this.hero.damage + this.hero.weapon.damage <= 0) {
+        return 1;
+      }
+      return this.hero.damage + this.hero.weapon.damage;
+    }
+    return this.hero.damage;
+  }
+
+  getSpeed() {
+    if (this.hero.weapon?.speed !== 0) {
+      if (this.hero.speed + this.hero.weapon.speed <= 0) {
+        return 1;
+      }
+      return this.hero.speed + this.hero.weapon.speed;
+    }
+    return this.hero.speed;
+  }
+
+  getDodge() {
+    if (this.hero.weapon?.dodge !== 0) {
+      if (this.hero.dodge + this.hero.weapon.dodge <= 0) {
+        return 1;
+      }
+      return this.hero.dodge + this.hero.weapon.dodge;
+    }
+    return this.hero.dodge;
+  }
+
+  getHealth() {
+    if (this.hero.weapon?.health !== 0) {
+      if (this.hero.health + this.hero.weapon.health <= 0) {
+        return 1;
+      }
+      return this.hero.health + this.hero.weapon.health;
+    }
+    return this.hero.health;
+  }
 }
