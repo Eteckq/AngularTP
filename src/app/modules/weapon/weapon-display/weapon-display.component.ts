9c9c2728b10901eb2d022c43ccec2cdfd37e31a3
Yohan <yohan.g99@gmail.com>
2021-03-31 14:25:04 +0200

    weapon

--- src/app/modules/weapon/weapon-display/weapon-display.component.ts
+++ src/app/modules/weapon/weapon-display/weapon-display.component.ts
@@ -1,26 +1,13 @@
-import {Component, Input, OnInit} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
-import {WeaponsService} from "../../../shared/services/weapons.service";
-import {Location} from "@angular/common";
-import {Weapon} from "../../../shared/data/weapon";
+import { Component, Input } from '@angular/core';
+import { Weapon } from '../../../shared/data/weapon';
 
 @Component({
   selector: 'app-weapon-display',
   templateUrl: './weapon-display.component.html',
   styleUrls: ['./weapon-display.component.scss'],
 })
-
-export class WeaponDisplayComponent implements OnInit {
+export class WeaponDisplayComponent {
   @Input() weapon: Weapon;
 
-
-  constructor(
-    private route: ActivatedRoute,
-    private weaponesService: WeaponsService,
-    private location: Location
-  ) {}
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
