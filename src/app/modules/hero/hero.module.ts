4805f1fac6e35ac932a2e03550b42af9f5c79bd2
Yohan <yohan.g99@gmail.com>
2021-03-31 10:26:29 +0200

    hero display

--- src/app/modules/hero/hero.module.ts
+++ src/app/modules/hero/hero.module.ts
@@ -1,13 +1,13 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { AppRoutingModule } from "../../app-routing.module";
-import { CreateHeroComponent } from "./create-hero/create-hero.component";
-import { EditHeroComponent } from "./edit-hero/edit-hero.component";
-import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
-import { HeroEditorComponent } from "./hero-editor/hero-editor.component";
-import { HeroesComponent } from "./heroes/heroes.component";
-
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../../app-routing.module';
+import { CreateHeroComponent } from './create-hero/create-hero.component';
+import { EditHeroComponent } from './edit-hero/edit-hero.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroEditorComponent } from './hero-editor/hero-editor.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +16,7 @@ import { HeroesComponent } from "./heroes/heroes.component";
     CreateHeroComponent,
     EditHeroComponent,
     HeroEditorComponent,
+    HeroComponent,
   ],
   imports: [CommonModule, FormsModule, AppRoutingModule],
 })
